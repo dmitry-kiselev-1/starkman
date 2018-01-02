504d67c2ffdcb5f61c40970aae812ca50868f0f3
Dmitry Kiselev <kd.000.000.1@gmail.com>
2018-01-02 08:11:54 +0300

    JSON NullValueHandling

--- backend/Starkman.Backend.WebApi/Startup.cs
+++ backend/Starkman.Backend.WebApi/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 using Starkman.Backend.Domain.Entities.Seo;
 using Starkman.Backend.Domain.Services;
 using Starkman.Backend.Domain.Services.Redis;
@@ -27,9 +29,17 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Add(ServiceDescriptor.Transient<IStorageService<Category>, RedisCategoryStorageService>());
+            services.AddResponseCompression();
 
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                });
+
+            // Dependency Injection:
+            services.Add(ServiceDescriptor.Transient<IStorageService<Category>, RedisCategoryStorageService>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +49,8 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseResponseCompression();
 
             app.UseMvc();
 

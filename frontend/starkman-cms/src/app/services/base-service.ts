ea1c4ef761b0a22920f01a1fd699d48637c15106
Dmitry Kiselev <kd.000.000.1@gmail.com>
2018-01-07 00:49:18 +0300

    *

--- frontend/starkman-cms/src/app/services/base-service.ts
+++ frontend/starkman-cms/src/app/services/base-service.ts
@@ -7,22 +7,14 @@ export abstract class BaseService {
   protected apiRoutePrefix: string;
 
   constructor(
-    protected apiDomain: string = 'http://localhost:8082', /* 'http://138.68.185.190:3000', */
-    protected uiDomain: string = 'http://localhost:4200',  /* 'http://138.68.185.190:4200', */
+    protected apiDomain: string = 'http://localhost:8082', /* 'http://138.68.185.190:3000' */
     protected requestOptions = new RequestOptions(),
     private headers =
       new Headers(
         {
           'Content-Type': 'application/json; charset=utf-8',
           'X-Requested-With': 'XMLHttpRequest',
-          'X-XSRF-TOKEN': '83ce7cb9-a84d-4560-946b-13602bf13ec1',
-          'Access-Control-Allow-Origin': uiDomain,
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-          'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
-          'Access-Control-Allow-Credentials': true,
-          /*'Origin': uiDomain*/
-          /*Access-Control-Request-Method: POST*/
-          /*Access-Control-Request-Headers: X-Custom-Header*/
+          'X-XSRF-TOKEN': '83ce7cb9-a84d-4560-946b-13602bf13ec1'
         })
   ) {
     this.requestOptions.headers = this.headers;

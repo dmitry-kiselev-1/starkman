cb942e108cd2a3fb8bb47e9923a8345e9e57dd12
Dmitry Kiselev <kd.000.000.1@gmail.com>
2018-01-08 10:36:08 +0300

    *

--- frontend/starkman-cms/src/app/components/photo/photo-form/photo-form.component.ts
+++ frontend/starkman-cms/src/app/components/photo/photo-form/photo-form.component.ts
@@ -48,7 +48,7 @@ export class PhotoFormComponent extends BaseComponent implements OnInit {
       });
   }
 
-  download()
+  downloadFile()
   {
     /*
     if (!this.entity.Url) return;
@@ -67,8 +67,35 @@ export class PhotoFormComponent extends BaseComponent implements OnInit {
     */
   }
 
-  upload()
+  // https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+  uploadFile(files)
   {
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+
+      if (!file.type.startsWith('image/')){ continue }
+
+      var img = document.createElement("img");
+      img.classList.add("obj");
+      img.src = file;
+      $("#preview").append(img);
+
+      var reader = new FileReader();
+      reader.onload =
+      (
+        function(aImg)
+        {
+          return function(e)
+          {
+            aImg.src = e.target.result;
+          };
+        }
+      )
+      (img);
+
+      reader.readAsDataURL(file);
+    }
+
     /*
     if (!this.entity.Url) return;
 

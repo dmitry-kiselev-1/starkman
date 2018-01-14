1a4e8d985bdbc72686196c07109478c18cba28ce
Dmitry Kiselev <kd.000.000.1@gmail.com>
2018-01-14 22:05:56 +0300

    *

--- backend/Starkman.Backend.xUnitTest/Storage/StorageTest.cs
+++ backend/Starkman.Backend.xUnitTest/Storage/StorageTest.cs
@@ -18,7 +18,40 @@ namespace Starkman.Backend.xUnitTest.Storage
         {
             var categoryList = new List<Category>()
             {
-                new Category() { Url = "bryuki_casual", Title = "Брюки Casual", IsVisible = true, SortOrder = 1 },
+                new Category()
+                {
+                    Url = "bryuki_casual", Title = "Брюки Casual", IsVisible = true, SortOrder = 1,
+                    Photo = new Photo() { Type = "jpeg", Size = 370661, Url = "bryuki_casual", IsVisible = false, IsDeleted = false },
+                    ProductList = new List<Product>()
+                    {
+                        new Product(){
+                            UrlParent = "bryuki_casual", Url = "bryuki_casual_100", Sku = 100,
+                            Title = "Брюки классика", Description = "Брюки классика Description",
+                            MetaKeywords = "Брюки классика MetaKeywords", MetaDescription = "Брюки классика MetaDescription",
+                            IsVisible = true, SortOrder = 2, IsDeleted = false,
+                            PhotoList = new List<Photo>()
+                            {
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "bryuki_casual_100_v1", IsVisible = true, IsDeleted = false },
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "bryuki_casual_100_v2", IsVisible = true, IsDeleted = false },
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "bryuki_casual_100_v3", IsVisible = true, IsDeleted = false }
+                            }
+                        },
+                        new Product(){
+                            UrlParent = "kostyumy_detskie", Url = "kostyumy_detskie_200", Sku = 200,
+                            Title = "Костюмы для мальчиков", Description = "Костюмы для мальчиков Description",
+                            MetaKeywords = "Костюмы для мальчиков MetaKeywords", MetaDescription = "Костюмы для мальчиков MetaDescription",
+                            IsVisible = true, SortOrder = 2, IsDeleted = false,
+                            PhotoList = new List<Photo>()
+                            {
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "kostyumy_detskie_200_v1", IsVisible = true, IsDeleted = false },
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "kostyumy_detskie_200_v2", IsVisible = true, IsDeleted = false },
+                                new Photo() { Type = "jpeg", Size = 370661, Url = "kostyumy_detskie_200_v3", IsVisible = true, IsDeleted = false }
+                            }
+                        }
+
+                    }
+                },
+
                 new Category() { Url = "bryuki_klassika", Title = "Брюки классика", IsVisible = true, SortOrder = 2 },
                 new Category() { Url = "bryuki_zauzhennye", Title = "Брюки зауженные", IsVisible = true, SortOrder = 3 },
                 new Category() { Url = "bryuki_detskie", Title = "Брюки детские", IsVisible = true, SortOrder = 4 },

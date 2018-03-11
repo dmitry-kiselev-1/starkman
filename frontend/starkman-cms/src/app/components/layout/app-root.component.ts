aa36a3a76666aa03ce1ac790a4143eb24cbfbd1c
Dmitry Kiselev <v-dmkise@microsoft.com>
2018-03-11 18:01:19 +0300

    исправлено смещение полосы процесса

--- frontend/starkman-cms/src/app/components/layout/app-root.component.ts
+++ frontend/starkman-cms/src/app/components/layout/app-root.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, Input, OnInit, AfterContentChecked, AfterViewInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnInit, AfterContentChecked, AfterViewInit, ChangeDetectionStrategy} from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
 import {BaseComponent} from '../base.component';
 import {NotificationService} from '../../services/notification.service';
@@ -6,12 +6,13 @@ import {NotificationService} from '../../services/notification.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app-root.component.html',
-  styleUrls: ['./app-root.component.css']
+  styleUrls: ['./app-root.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class AppRootComponent implements OnInit {
 
   public appTitle = 'Starkman CMS';
-  public appLoading = false;
+  public appLoading = true;
   public progressBarMode: string = "determinate";
 
   mobileQuery: MediaQueryList;
@@ -24,24 +25,16 @@ export class AppRootComponent implements OnInit {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
+  ngOnInit(): void {
     this.notificationService.appLoadingChange
       .subscribe((state) => {
-        this.appLoading = state
-        this.progressBarMode = this.getProgressBarMode(state);
+        if (this.appLoading != state) {
+          this.appLoading = state;
+          this.progressBarMode = this.appLoading ? "buffer" : "determinate";
+        }
       });
-  }
-
-  getProgressBarMode(state: boolean): string
-  {
-    return state ? "buffer" : "determinate";
-  }
-
-  ngOnInit(): void {}
-
-  // https://blog.angularindepth.com/everything-you-need-to-know-about-the-expressionchangedafterithasbeencheckederror-error-e3fd9ce7dbb4
-  ngAfterViewInit() {
-    this.changeDetectorRef.detectChanges();
   }
 
 }
